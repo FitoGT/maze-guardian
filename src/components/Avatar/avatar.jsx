e421df28df876307bdba6d938f86b7ec51f8f8c5
Adolfo <adolfo.guevara.11@gmail.com>
2021-06-22 23:57:01 -0300

    connecting avatar with redux

--- src/components/Avatar/avatar.jsx
+++ src/components/Avatar/avatar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './avatar.css'
-const Avatar = () =>(
-    <div className="avatar">
-        <div className="dialog-1">
-            <div className="left-point"></div>
-            Good Job! you've done moves!
+import { connect } from 'react-redux'
+const Avatar = ({ count }) => {
+    return (
+        <div className="avatar">
+            <div className="dialog-1">
+                <div className="left-point"></div>
+                {`Good Job! you've done ${count.count} moves!`}
+            </div>
         </div>
-    </div>
-)
+    )
+}
 
-export default Avatar 
+
+const mapStateToProps = ({ count }) => ({
+    count: count
+})
+
+export default connect(mapStateToProps)(Avatar)
